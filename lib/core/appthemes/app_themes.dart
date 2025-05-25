5aa110576515ec1f806819180d5dddf931b1f0ac
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2025-05-25 15:11:27 +0800

    Studio Page Completed

--- lib/core/appthemes/app_themes.dart
+++ lib/core/appthemes/app_themes.dart
@@ -9,9 +9,9 @@ class ThemeNotifier extends StateNotifier<bool> {
         surface: Colors.white,
         primary: const Color(0xff196EEE),
         secondary: const Color(0xff2DD7A4),
-        tertiary: const Color.fromARGB(255, 180, 180, 180).withOpacity(0.2),
+        tertiary: const Color(0xffB47820),
         inversePrimary: Colors.grey.shade900,
-        tertiaryContainer: const Color(0xff939cc4),
+        tertiaryContainer: const Color(0xffDF9652),
         primaryFixedDim: Colors.black),
   );
 
