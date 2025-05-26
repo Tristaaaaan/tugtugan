4f00a47eb85ab4ee2a23d5d8b8cbdfbbba67668a
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2025-05-26 17:11:24 +0800

    Home Page Setup

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:tugtugan/config/app_config.dart';
 import 'package:tugtugan/config/app_environments.dart';
 import 'package:tugtugan/core/appthemes/app_themes.dart';
 import 'package:tugtugan/features/authentication/auth_gate.dart';
+import 'package:tugtugan/features/maps/maps.dart';
 import 'package:tugtugan/firebase/prod/firebase_options.dart';
 
 void main() async {
@@ -27,6 +29,27 @@ void main() async {
   runApp(const ProviderScope(child: MainApp()));
 }
 
+/// The route configuration.
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const AuthGate();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'maps',
+          builder: (BuildContext context, GoRouterState state) {
+            final name = state.uri.queryParameters['name'] ?? 'Guest';
+            return Maps(name: name);
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
 class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
@@ -36,13 +59,9 @@ class MainApp extends ConsumerWidget {
 
     final theme = ref.read(themeNotifierProvider.notifier).currentTheme;
 
-    return MaterialApp(
+    return MaterialApp.router(
       theme: theme,
-      home: const Scaffold(
-        body: Center(
-          child: AuthGate(),
-        ),
-      ),
+      routerConfig: _router,
     );
   }
 }
