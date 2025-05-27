b57a0e2a27186b7e6b70cc448bcce4da65dea312
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2025-05-27 17:06:56 +0800

    Preparing adding of studio page

--- lib/features/authentication/auth_gate.dart
+++ lib/features/authentication/auth_gate.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tugtugan/features/authentication/signin.dart';
-import 'package:tugtugan/features/home/home.dart';
+import 'package:tugtugan/features/registration/studio_registration.dart';
 
 final authStateProvider = StreamProvider<User?>((ref) {
   return FirebaseAuth.instance.authStateChanges();
@@ -18,7 +18,7 @@ class AuthGate extends ConsumerWidget {
     final user = authStateAsync.value;
 
     return Scaffold(
-      body: user != null ? const HomePage() : const SigninPage(),
+      body: user != null ? const StudioRegistration() : const SigninPage(),
     );
   }
 }
