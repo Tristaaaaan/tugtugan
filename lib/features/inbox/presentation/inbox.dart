47bc4cafcf80a25483e7c4704b550e05a3ac2fe1
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2025-06-02 18:12:00 +0800

    Implemented inbox screen

--- lib/features/inbox/presentation/inbox.dart
+++ lib/features/inbox/presentation/inbox.dart
@@ -1,7 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:shimmer/shimmer.dart';
 
 import 'provider/inbox_provider.dart';
 
@@ -11,7 +14,7 @@ class InboxPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final FirebaseAuth auth = FirebaseAuth.instance;
-    final inbox = ref.watch(inboxProvider(auth.currentUser!.uid));
+    final inbox = ref.watch(enrichedInboxProvider(auth.currentUser!.uid));
     return SafeArea(
       child: Scaffold(
           body: inbox.when(
@@ -24,23 +27,82 @@ class InboxPage extends ConsumerWidget {
                     final message = messages[index];
                     return GestureDetector(
                       onTap: () {
-                        context.go('/chat?studioId=${message.studioId}');
+                        context.go('/chat?studioId=${message.chat.studioId}');
                       },
                       child: Container(
-                        padding: const EdgeInsets.all(10),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 3),
                         margin: const EdgeInsets.symmetric(
                           vertical: 10,
                           horizontal: 25,
                         ),
-                        child: Row(children: [
-                          Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
                               children: [
-                                Text(message.clientId),
-                                Text(message.lastMessage),
-                              ]),
-                        ]),
+                                SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(50),
+                                    child: CachedNetworkImage(
+                                      imageUrl: message.studio!.imageUrl,
+                                      fit: BoxFit.cover,
+                                      errorWidget: (context, url, error) =>
+                                          const Icon(
+                                        Icons.broken_image,
+                                      ),
+                                      placeholder: (context, url) =>
+                                          Shimmer.fromColors(
+                                        baseColor: Colors.grey[400]!,
+                                        highlightColor: Colors.grey[300]!,
+                                        child: Container(
+                                          width: double.infinity,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(5),
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .tertiary,
+                                          ),
+                                          height: 150,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    ConstrainedBox(
+                                      constraints: BoxConstraints(
+                                        maxWidth:
+                                            MediaQuery.of(context).size.width *
+                                                0.5,
+                                      ),
+                                      child: IntrinsicHeight(
+                                        child: Text(
+                                          message.studio!.studioName,
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 2,
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 5),
+                                    Text(message.chat.lastMessage),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Text(
+                              DateFormat('hh:mm a').format(
+                                  message.chat.lastMessageTimeSent.toDate()),
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   })
