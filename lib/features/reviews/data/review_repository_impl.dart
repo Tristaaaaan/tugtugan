70fc32d4f658c94a8ae3e44ff409a3a6c28608b0
Mark Tristan Fabellar <112135724+Tristaaaaan@users.noreply.github.com>
2025-06-19 17:31:27 +0800

    feature: prepare fetching data in reviews

--- lib/features/reviews/data/review_repository_impl.dart
+++ lib/features/reviews/data/review_repository_impl.dart
@@ -1,15 +1,39 @@
+import 'dart:developer' as developer;
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import '../../../core/appmodels/review_model.dart';
 import '../domain/review_repository.dart';
 
 class ReviewRepositoryImpl extends ReviewRepository {
+  final FirebaseFirestore _firestore;
+  ReviewRepositoryImpl({FirebaseFirestore? firestore})
+      : _firestore = firestore ?? FirebaseFirestore.instance;
+
   @override
-  Future<void> addReview(ReviewModel reviewData) {
-    // TODO: implement addReview
-    throw UnimplementedError();
+  Future<void> addReview(ReviewModel reviewData) async {
+    try {
+      await _firestore
+          .collection("studios")
+          .doc(reviewData.studioId)
+          .collection("reviews")
+          .add(reviewData.toMap());
+    } catch (e, st) {
+      developer.log('Error adding review', error: e, stackTrace: st);
+      rethrow;
+    }
   }
 
+  @override
   Stream<List<ReviewModel>> streamReviews(String studioId) {
-    // TODO: implement streamReviews
-    throw UnimplementedError();
+    return _firestore
+        .collection("studios")
+        .doc(studioId)
+        .collection("reviews")
+        .orderBy("createdAt", descending: true)
+        .snapshots()
+        .map((snapshot) => snapshot.docs
+            .map((doc) => ReviewModel.fromMap(doc.data()))
+            .toList());
   }
 }
